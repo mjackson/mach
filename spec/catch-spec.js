562ba4e0e5fb20013399a92a3d3c976391a607b0
Michael Jackson <mjijackson@gmail.com>
2013-03-09 23:45:46 -0800

    Add mach.catch

---
+++ spec/catch-spec.js
@@ -0,0 +1,32 @@
+require('./helper');
+
+describe('mach.catch', function () {
+  describe('when an Error is thrown from downstream', function () {
+    var caughtError;
+    beforeEach(function () {
+      caughtError = null;
+      return callApp(mach.catch(function (request) {
+        throw new Error('boom!');
+      })).then(null, function (error) {
+        caughtError = error;
+      });
+    });
+
+    it('throws it', function () {
+      assert(caughtError);
+    });
+  });
+
+  describe('when a non-Error is thrown from downstream', function () {
+    beforeEach(function () {
+      return callApp(mach.catch(function (request) {
+        throw 404;
+      }));
+    });
+
+    it('returns it', function () {
+      assert(lastResponse);
+      assert.equal(lastResponse.status, 404);
+    });
+  });
+});
