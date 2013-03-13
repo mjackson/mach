e4b76a01ae5be9ca0b193e8dc0fb3817e3e73af2
Michael Jackson <mjijackson@gmail.com>
2013-03-12 18:18:14 -0700

    Add large file spec

--- spec/file-spec.js
+++ spec/file-spec.js
@@ -4,10 +4,24 @@ var fs = require('fs');
 var file = mach.file;
 
 describe('mach.file', function () {
-  var filename = path.basename(__filename);
-  var contents = fs.readFileSync(__filename, 'utf8');
+  describe('when a large file is requested', function () {
+    var fullPath = __dirname + '/_files/jquery-1.8.3.js';
+    var filename = path.basename(fullPath);
+    var contents = fs.readFileSync(fullPath, 'utf8');
+    var app = file(path.dirname(fullPath));
+
+    beforeEach(function () {
+      return callApp(app, '/' + filename);
+    });
+
+    it('serves the file', function () {
+      assert.equal(lastResponse.buffer, contents);
+    });
+  });
 
   describe('with a single index file', function () {
+    var filename = path.basename(__filename);
+    var contents = fs.readFileSync(__filename, 'utf8');
     var app = file(__dirname, filename);
 
     describe('when a file is requested', function () {
@@ -68,7 +82,9 @@ describe('mach.file', function () {
   });
 
   describe('with multiple index files', function () {
-    var app = file(__dirname, ['index.html', filename]);
+    var filename = path.basename(__filename);
+    var contents = fs.readFileSync(__filename, 'utf8');
+    var app = file(__dirname, [ 'index.html', filename ]);
 
     describe('when a directory is requested', function () {
       beforeEach(function () {
