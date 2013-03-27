a3a0171f4e647b98ea8d5c4fef533ab254ba9abe
Michael Jackson <mjijackson@gmail.com>
2013-03-27 14:47:53 -0700

    Auto-calculate Last-Modified and ETag in mach.file

--- prototypes/file-server.js
+++ prototypes/file-server.js
@@ -3,6 +3,11 @@ var app = mach.stack();
 
 app.use(mach.gzip);
 app.use(mach.commonLogger);
-app.use(mach.file, __dirname + '/..');
+app.use(mach.modified);
+app.use(mach.file, {
+  root: __dirname + '/..',
+  useLastModified: true,
+  useEtag: true
+});
 
 mach.serve(app);
