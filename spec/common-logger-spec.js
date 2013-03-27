a722dd3aed6154b6df90fff9be4bdfd933b9b106
Michael Jackson <mjijackson@gmail.com>
2013-03-27 14:46:52 -0700

    Log length of 0

---
+++ spec/common-logger-spec.js
@@ -0,0 +1,24 @@
+require('./helper');
+
+describe('mach.commonLogger', function () {
+  describe('when a response has Content-Length of 0', function () {
+    var app, log;
+    beforeEach(function () {
+      log = {};
+      app = mach.commonLogger(function (request) {
+        return { 'Content-Length': 0 };
+      }, fakeStream(log));
+
+      return callApp(app);
+    });
+
+    it('logs a content length of 0', function () {
+      assert(log.data);
+      var entries = log.data.split('\n');
+      var match = entries[0].match(/\b(\d+)\b$/);
+      assert(match);
+      var contentLength = match[1];
+      assert.equal(contentLength, '0');
+    });
+  });
+});
