8671365afbbe217bae546c84569cd8d87aa25415
Michael Jackson <mjijackson@gmail.com>
2013-03-27 15:11:39 -0700

    Fix failing specs

--- spec/file-spec.js
+++ spec/file-spec.js
@@ -1,14 +1,13 @@
 require('./helper');
 var path = require('path');
 var fs = require('fs');
-var file = mach.file;
 
 describe('mach.file', function () {
   describe('when a large file is requested', function () {
     var fullPath = specFile('jquery-1.8.3.js');
     var filename = path.basename(fullPath);
     var contents = fs.readFileSync(fullPath, 'utf8');
-    var app = file(path.dirname(fullPath));
+    var app = mach.file(path.dirname(fullPath));
 
     beforeEach(function () {
       return callApp(app, '/' + filename);
@@ -22,7 +21,10 @@ describe('mach.file', function () {
   describe('with a single index file', function () {
     var filename = path.basename(__filename);
     var contents = fs.readFileSync(__filename, 'utf8');
-    var app = file(__dirname, filename);
+    var app = mach.file({
+      root: __dirname,
+      index: filename
+    });
 
     describe('when a file is requested', function () {
       beforeEach(function () {
@@ -84,7 +86,10 @@ describe('mach.file', function () {
   describe('with multiple index files', function () {
     var filename = path.basename(__filename);
     var contents = fs.readFileSync(__filename, 'utf8');
-    var app = file(__dirname, [ 'index.html', filename ]);
+    var app = mach.file({
+      root: __dirname,
+      index: [ 'index.html', filename ]
+    });
 
     describe('when a directory is requested', function () {
       beforeEach(function () {
