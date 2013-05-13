0429f2815b831b03e00167a12e4fc931b708a7e9
Michael Jackson <mjijackson@gmail.com>
2013-05-13 08:12:14 -0700

    Add TTL to mach.session.CookieStore
    Also added specs

--- spec/describe-session-store.js
+++ spec/describe-session-store.js
@@ -1,5 +1,6 @@
 require('./helper');
 var when = require('when');
+var delay = require('when/delay');
 module.exports = describeSessionStore;
 
 function describeSessionStore(store) {
@@ -37,6 +38,75 @@ function describeSessionStore(store) {
       return when(store.load(value), function (session) {
         assert(session);
         assert.equal(session.count, 1);
+      });
+    });
+  });
+
+  describe('when it has a TTL', function () {
+    beforeEach(function () {
+      store.ttl = 10;
+    });
+
+    afterEach(function () {
+      delete store.ttl;
+    });
+
+    describe('and a session is not expired', function () {
+      var value;
+      beforeEach(function () {
+        var session = { count: 1 };
+        return when(store.save(session), function (newValue) {
+          value = newValue;
+        });
+      });
+
+      it('loads the session', function () {
+        return when(store.load(value), function (session) {
+          assert(session);
+          assert.equal(session.count, 1);
+        });
+      });
+    });
+
+    describe('and a session is expired', function () {
+      var value;
+      beforeEach(function () {
+        var session = { count: 1 };
+        return when(store.save(session), function (newValue) {
+          value = newValue;
+          return delay(store.ttl);
+        });
+      });
+
+      it('loads a new session', function () {
+        return when(store.load(value), function (session) {
+          assert(session);
+          assert.deepEqual(session, {});
+        });
+      });
+    });
+
+    describe('and a session is touched before it expires', function () {
+      var value;
+      beforeEach(function () {
+        var session = { count: 1 };
+        return when(store.save(session), function () {
+          return delay(store.ttl / 2).then(function () {
+            return when(store.touch(session), function () {
+              return when(store.save(session), function (newValue) {
+                value = newValue;
+                return delay(store.ttl / 2);
+              });
+            });
+          });
+        });
+      });
+
+      it('loads the session', function () {
+        return when(store.load(value), function (session) {
+          assert(session);
+          assert.equal(session.count, 1);
+        });
       });
     });
   });
