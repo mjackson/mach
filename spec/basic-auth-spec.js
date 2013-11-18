0d83bc96d88d94c1e43c8311af7f9f7946c884bf
Michael Jackson <mjijackson@gmail.com>
2013-11-18 10:58:12 -0800

    Add basic auth spec

---
+++ spec/basic-auth-spec.js
@@ -0,0 +1,61 @@
+require('./helper');
+
+describe('mach.basicAuth', function () {
+  function ok(request) {
+    return 200;
+  }
+
+  function validateCredentials(username, password) {
+    return username === 'michael' && password === 'password';
+  }
+
+  var app;
+  beforeEach(function () {
+    app = mach.basicAuth(ok, validateCredentials);
+  });
+
+  describe('when no authentication credentials are given', function () {
+    beforeEach(function () {
+      return callApp(app);
+    });
+
+    it('returns 401 Unauthorized', function () {
+      assert(lastResponse);
+      assert.equal(lastResponse.status, 401);
+    });
+  });
+
+  describe('when invalid authentication credentials are given', function () {
+    beforeEach(function () {
+      return callApp(app, {
+        headers: {
+          'Authorization': makeBasicAuthorization('michael', 'wrongPassword')
+        }
+      });
+    });
+
+    it('returns 401 Unauthorized', function () {
+      assert(lastResponse);
+      assert.equal(lastResponse.status, 401);
+    });
+  });
+
+  describe('when valid credentials are given', function () {
+    beforeEach(function () {
+      return callApp(app, {
+        headers: {
+          'Authorization': makeBasicAuthorization('michael', 'password')
+        }
+      });
+    });
+
+    it('returns passes the app downstream', function () {
+      assert(lastResponse);
+      assert.equal(lastResponse.status, 200);
+    });
+  });
+});
+
+function makeBasicAuthorization(username, password) {
+  return 'Basic ' + mach.utils.encodeBase64([ username, password ].join(':'));
+}
