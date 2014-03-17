33dfb168a06feee66498b7606640543cf4c4734d
Michael Jackson <mjijackson@gmail.com>
2014-03-16 17:13:48 -0700

    Update mach.token and docs

--- modules/middleware/token.js
+++ modules/middleware/token.js
@@ -1,4 +1,3 @@
-var crypto = require('crypto');
 var utils = require('../utils');
 
 /**
@@ -20,44 +19,54 @@ var utils = require('../utils');
  * mach.token in order for it to be able to retrieve values from the request session
  * and parameters, like this:
  *
- *   app.use(mach.session, {
- *     secret: 'ffefc1195648a5583cd4779a626c8b12'
- *   });
+ *   app.use(mach.session);
  *   app.use(mach.params);
  *   app.use(mach.token);
  *   app.run(function (request) {
  *     // The request was authenticated successfully.
  *   });
+ *
+ * Options may be any of the following:
+ *
+ *   - paramName        The name of the request parameter that contains the token
+ *                      (i.e. the value of the "name" attribute on your <input>).
+ *                      Defaults to "_token"
+ *   - sessionKey       The name of the session variable to use to store the token.
+ *                      Defaults to "_token"
+ *   - byteLength       The length of the token in bytes. Defaults to 32
  */
-module.exports = function (app, paramName, sessionKey, byteLength) {
-  byteLength = byteLength || 32;
-  sessionKey = sessionKey || '_token';
-  paramName = paramName || '_token';
+module.exports = function (app, options) {
+  options = options || {};
+
+  if (typeof options === 'string')
+    options = { paramName: options };
+
+  var paramName = options.paramName || '_token';
+  var sessionKey = options.sessionKey || '_token';
+  var byteLength = options.byteLength || 32;
 
   return function (request) {
     var session = request.session;
+    var params = request.params;
+
     if (!session) {
       request.error.write('No request session. Use mach.session in front of mach.token\n');
-      session = {};
-    }
+    } else if (!params) {
+      request.error.write('No request params. Use mach.params in front of mach.token\n');
+    } else {
+      // Get the session token, creating a new one if needed.
+      var token = session[sessionKey];
+      if (!token)
+        token = session[sessionKey] = utils.makeToken(byteLength);
 
-    var token = session[sessionKey];
-    if (!token) {
-      var buffer = crypto.randomBytes(byteLength);
-      token = session[sessionKey] = buffer.toString('hex');
+      if (params[paramName] && params[paramName] === token)
+        return request.call(app);
     }
 
     // If the request is not a POST we assume it's not a form submission
     // and therefore not modifying anything. Pass it downstream.
     if (utils.isSafeRequestMethod(request.method))
       return request.call(app);
-
-    var params = request.params;
-    if (!params) {
-      request.error.write('No request params. Use mach.params in front of mach.token\n');
-    } else if (params[paramName] && params[paramName] === token) {
-      return request.call(app);
-    }
 
     return utils.forbidden();
   };
