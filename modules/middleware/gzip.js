21453001f467ff586b0240c7ac880cc3ffe25bbd
Michael Jackson <mjijackson@gmail.com>
2014-03-17 10:34:32 -0700

    Ensure client accepts gzip encoding
    Also, removed superfluous `test` option.

--- modules/middleware/gzip.js
+++ modules/middleware/gzip.js
@@ -3,13 +3,8 @@ var utils = require('../utils');
 module.exports = Gzip;
 
 /**
- * A middleware that encodes the body of the response using Gzip. The options
- * may be any of node's zlib options (see http://nodejs.org/api/zlib.html) or
- * any of the following:
- *
- *   - test   A function that tests the response to see if it should be gzip'd
- *            or not. Should return a boolean. Defaults to checking the value of
- *            the Content-Type header for values that are able to be compressed.
+ * A middleware that encodes the body of the response using Gzip. Options
+ * may be any of node's zlib options (see http://nodejs.org/api/zlib.html).
  */
 function Gzip(app, options) {
   if (!(this instanceof Gzip))
@@ -17,39 +12,30 @@ function Gzip(app, options) {
 
   options = options || {};
 
-  var gzipTest;
-  if (typeof options.test === 'function') {
-    gzipTest = options.test;
-    delete options.test;
-  } else {
-    gzipTest = canGzip;
-  } 
-
   this._app = app;
-  this._gzipTest = gzipTest;
-  this._gzipOptions = options;
+  this._options = options;
 }
 
 Gzip.prototype.apply = function (request) {
   return request.call(this._app).then(function (response) {
-    if (this._gzipTest(response)) {
-      response.content = this.wrapContent(response.content);
-      response.headers['Content-Encoding'] = 'gzip';
-      response.headers['Vary'] = 'Accept-Encoding';
-      delete response.headers['Content-Length'];
-    }
+    if (!canGzipContentType(response.headers['Content-Type']) || !request.acceptsEncoding('gzip'))
+      return response;
+
+    response.content = this.wrapContent(response.content);
+    response.headers['Content-Encoding'] = 'gzip';
+    response.headers['Vary'] = 'Accept-Encoding';
+
+    delete response.headers['Content-Length'];
 
     return response;
   }.bind(this));
 };
 
 Gzip.prototype.wrapContent = function (content) {
-  return content.pipe(zlib.createGzip(this._gzipOptions));
+  return content.pipe(zlib.createGzip(this._options));
 };
 
-function canGzip(response) {
-  var contentType = response.headers['Content-Type'];
-
+function canGzipContentType(contentType) {
   if (!contentType || contentType === 'text/event-stream')
     return false;
 
