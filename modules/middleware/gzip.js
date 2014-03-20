8b3ad01afaf9b8ab22b6ca3c46fa7c5893e5a7e7
Michael Jackson <mjijackson@gmail.com>
2014-03-19 18:50:36 -0700

    Avoid the use of bind

--- modules/middleware/gzip.js
+++ modules/middleware/gzip.js
@@ -1,43 +1,34 @@
 var zlib = require('zlib');
 var utils = require('../utils');
-module.exports = Gzip;
 
 /**
  * A middleware that gzip's the response content (see http://www.gzip.org/).
  * Options may be any of node's zlib options (see http://nodejs.org/api/zlib.html).
  */
-function Gzip(app, options) {
-  if (!(this instanceof Gzip))
-    return new Gzip(app, options);
+module.exports = function (app, gzipOptions) {
+  return function (request) {
+    return request.call(app).then(function (response) {
+      var headers = response.headers;
 
-  options = options || {};
+      if (!shouldGzipContentType(headers['Content-Type']) || !request.acceptsEncoding('gzip'))
+        return response;
 
-  this._app = app;
-  this._options = options;
-}
+      response.content = response.content.pipe(zlib.createGzip(gzipOptions));
 
-Gzip.prototype.apply = function (request) {
-  return request.call(this._app).then(function (response) {
-    if (!shouldGzipContentType(response.headers['Content-Type']) || !request.acceptsEncoding('gzip'))
+      headers['Content-Encoding'] = 'gzip';
+      headers['Vary'] = 'Accept-Encoding';
+      delete headers['Content-Length'];
+
       return response;
-
-    response.content = this.wrapContent(response.content);
-    response.headers['Content-Encoding'] = 'gzip';
-    response.headers['Vary'] = 'Accept-Encoding';
-
-    delete response.headers['Content-Length'];
-
-    return response;
-  }.bind(this));
+    });
+  };
 };
 
-Gzip.prototype.wrapContent = function (content) {
-  return content.pipe(zlib.createGzip(this._options));
-};
+var gzipTypes = /text|javascript|json/i;
 
 function shouldGzipContentType(contentType) {
   if (!contentType || contentType === 'text/event-stream')
     return false;
 
-  return (/text|javascript|json/i).test(contentType);
+  return gzipTypes.test(contentType);
 }
--- modules/middleware/logger.js
+++ modules/middleware/logger.js
@@ -1,44 +1,37 @@
 var strftime = require('strftime').strftime;
 var microtime = require('microtime');
-module.exports = Logger;
 
 /**
  * A middleware that writes log entry data about the response to a given stream.
  * Log entries are formatted similarly to Apache httpd's Common Log Format
  * (see http://httpd.apache.org/docs/1.3/logs.html#common).
  */
-function Logger(app, outputStream) {
-  if (!(this instanceof Logger))
-    return new Logger(app, outputStream);
+module.exports = function (app, outputStream) {
+  outputStream = outputStream || process.stderr;
 
-  this._app = app;
-  this._stream = outputStream || process.stderr;
-}
+  return function (request) {
+    var start = microtime.now();
 
-Logger.prototype.apply = function (request) {
-  var start = microtime.now();
+    return request.call(app).then(function (response) {
+      var host = request.remoteHost || '-';
+      var id = '-'; // RFC 1413 identity of the client determined by identd on the client's machine
+      var user = request.remoteUser || '-';
+      var timestamp = '[' + strftime('%d/%b/%Y %H:%M:%S', request.date) + ']';
+      var info = '"' + request.method + ' ' + request.fullPath + ' HTTP/' + request.protocolVersion + '"';
 
-  return request.call(this._app).then(function (response) {
-    this.createEntry(request, response, microtime.now() - start);
-    return response;
-  }.bind(this));
-};
+      var contentLength = response.headers['Content-Length'];
+      if (contentLength == null)
+        contentLength = '-';
 
-Logger.prototype.createEntry = function (request, response, elapsedMicroseconds) {
-  var host = request.remoteHost || '-';
-  var id = '-'; // RFC 1413 identity of the client determined by identd on the client's machine
-  var user = request.remoteUser || '-';
-  var timestamp = '[' + strftime('%d/%b/%Y %H:%M:%S', request.date) + ']';
-  var info = '"' + request.method + ' ' + request.fullPath + ' HTTP/' + request.protocolVersion + '"';
+      var elapsedMicroseconds = microtime.now() - start;
+      var seconds = Math.round(elapsedMicroseconds / 100) / 10000;
 
-  var contentLength = response.headers['Content-Length'];
-  if (contentLength == null)
-    contentLength = '-';
+      // 127.0.0.1 - frank [10/Oct/2000 13:55:36] "GET /apache_pb.gif HTTP/1.0" 200 2326 0.003
+      var entry = [ host, id, user, timestamp, info, response.status, contentLength, seconds ].join(' ');
 
-  var roundedSeconds = Math.round(elapsedMicroseconds / 100) / 10000;
+      outputStream.write(entry + '\n');
 
-  // 127.0.0.1 - frank [10/Oct/2000 13:55:36] "GET /apache_pb.gif HTTP/1.0" 200 2326 0.003
-  var entry = [ host, id, user, timestamp, info, response.status, contentLength, roundedSeconds ].join(' ');
-
-  this._stream.write(entry + '\n');
+      return response;
+    });
+  };
 };
