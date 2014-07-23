76dee8ca8d485f3a83acd9342d5ed615b17af494
Alex Buchanan <buchanae@gmail.com>
2014-07-23 19:58:11 +1200

    fix: allow calling setCookie() multiple times

---
+++ spec/message-spec.js
@@ -0,0 +1,39 @@
+require('./helper');
+var Message = mach.Message;
+
+describe('A mach.Message', function () {
+  var message;
+  beforeEach(function () {
+    message = new Message;
+  });
+
+  describe('addHeader', function () {
+
+    it('normalizes header names', function () {
+      message.addHeader('content-type', 'text/html');
+      expect(message.headers['Content-Type']).toEqual('text/html');
+    });
+
+    describe('when the header has not been previously set', function () {
+      it('sets the header to the given value', function () {
+        message.addHeader('Test', 'value');
+        expect(message.headers['Test']).toEqual('value');
+
+        message.addHeader('Test-Int', 1);
+        expect(message.headers['Test-Int']).toEqual(1);
+      });
+    });
+
+    describe('when the header has been previously set', function () {
+      beforeEach(function () {
+        message.addHeader('Test', 'previousValue');
+      });
+
+      it('sets the header to an array of header values', function () {
+        message.addHeader('Test', 'value');
+        expect(message.headers['Test']).toEqual(['previousValue', 'value']);
+      });
+
+    });
+  });
+});
---
+++ spec/response-spec.js
@@ -0,0 +1,35 @@
+require('./helper');
+var Response = mach.Response;
+
+describe('A mach.Response', function () {
+  var response;
+  beforeEach(function () {
+    response = new Response;
+  });
+
+  describe('setCookie', function () {
+    describe('when no cookies have been previously set', function () {
+      it('sets the "Set-Cookie" header to the appropriate string', function () {
+        response.setCookie('cookieName', {value: 'cookieValue'});
+
+        expect(response.headers['Set-Cookie']).toEqual('cookieName=cookieValue');
+      });
+    });
+
+    describe('when cookies have been previously set', function () {
+      beforeEach(function () {
+        response.setCookie('previousOne', {value: 'previousOneValue'});
+      });
+
+      it('sets the "Set-Cookie" header to an array of headers', function () {
+        response.setCookie('cookieName', {value: 'cookieValue'});
+
+        expect(response.headers['Set-Cookie']).toEqual([
+          'previousOne=previousOneValue',
+          'cookieName=cookieValue',
+        ]);
+      });
+
+    });
+  });
+});
