6c51db07b463cc1595db4453fe2d71dea89d3931
Michael Jackson <mjijackson@gmail.com>
2014-08-07 08:52:24 -0700

    Clean up parseURL implementation

--- modules/utils/parseURL.js
+++ modules/utils/parseURL.js
@@ -1,41 +1,6 @@
-function getURLProperties(object) {
-  return {
-    protocol: object.protocol,
-    slashes: true,
-    auth: object.username || object.password ? (object.username + ':' + object.password) : '',
-    host: object.host,
-    port: object.port,
-    hostname: object.hostname,
-    hash: object.hash,
-    search: object.search,
-    query: object.search.substring(1),
-    pathname: object.pathname,
-    path: object.pathname + object.search,
-    href: object.href
-  }; 
-}
-
-var URL;
-
-function parseURLNatively(url) {
-  return getURLProperties(new URL(url));
-}
-
-function parseURLUsingDOM(url) {
-  var a = document.createElement('a');
-  a.href = url;
-  return getURLProperties(a);
-}
-
 var parseURL;
 if (typeof window !== 'undefined') {
-  URL = window.URL || window.webkitURL;
-
-  if (typeof URL === 'function') {
-    parseURL = parseURLNatively;
-  } else {
-    parseURL = parseURLUsingDOM;
-  }
+  parseURL = require('./parseURLUsingDOM');
 } else {
   var moduleID = 'url'; // Stop Browserify.
   parseURL = require(moduleID).parse;
