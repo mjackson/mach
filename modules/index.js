649b48569094b6d3804097f9c704190092e4436e
Michael Jackson <mjijackson@gmail.com>
2014-08-09 00:48:29 -0700

    Remove warning message

--- modules/index.js
+++ modules/index.js
@@ -8,12 +8,3 @@ mach.version = '0.12.0';
 mach.Message = require('./Message');
 mach.Request = require('./Request');
 mach.Response = require('./Response');
-
-mach.serve = function () {
-  console.warn(
-    'require("mach").serve is deprecated. To run mach as a web server you should require("mach/server") instead.'
-  );
-
-  var moduleID = './server'; // Stop Browserify.
-  return require(moduleID).serve.apply(this, arguments);
-};
