7a5e5aa736cd14604ec9a32793918898a07391f1
Michael Jackson <mjijackson@gmail.com>
2014-08-17 07:10:16 -0700

    Add Accept* tests

--- specs/browser/tests.js
+++ specs/browser/tests.js
@@ -1,3 +1,8 @@
 require('../Message-spec');
 require('../Request-spec');
 require('../Response-spec');
+
+require('../accept/Accept-spec');
+require('../accept/AcceptCharset-spec');
+require('../accept/AcceptEncoding-spec');
+require('../accept/AcceptLanguage-spec');
