bdf091ef1d1511687028c496ab317867d9aa16f1
Michael Jackson <mjijackson@gmail.com>
2014-08-27 10:36:47 -0700

    Clean up route matching

--- modules/server/utils/compileRoute.js
+++ modules/server/utils/compileRoute.js
@@ -1,35 +1,30 @@
+var paramMatcher = /:([a-zA-Z_$][a-zA-Z0-9_$]*)|[*.()\[\]\\+|{}^$]/g;
+
 /**
- * Compiles the given route string into a RegExp that can be used to match
- * it. The route may contain named keys in the form of a colon followed by a
- * valid JavaScript identifier (e.g. ":name", ":_name", or ":$name" are all
- * valid keys). If the route contains the special "*" symbol, it is substituted
- * with a "(.*?)" pattern in the resulting RegExp.
+ * Compiles the given route string into a RegExp that can be used to
+ * match a URL. The route may contain named parameters in the form of
+ * a colon followed by a valid JavaScript identifier (e.g. ":name",
+ * ":_name", and ":$name" are all valid parameters). The route may
+ * also contain a * to match any character non-greedily, or a ? to
+ * match the previous thing 0 or 1 time.
  *
- * If the keys array is supplied, it is populated with the names of all keys.
+ * The keys array is populated with names of all parameters in the
+ * order they appear in the route string.
  */
 function compileRoute(route, keys) {
-  var pattern = route.replace(/:([a-zA-Z_$][a-zA-Z0-9_$]*)|[*.()^$]/g, function (match, key) {
-    switch (match) {
-    case '*':
-      if (keys)
-        keys.push('splat');
-
+  var source = route.replace(paramMatcher, function (match, key) {
+    if (key) {
+      keys.push(key);
+      return '([^./?#]+)';
+    } else if (match === '*') {
+      keys.push('splat');
       return '(.*?)';
-    case '.':
-    case '(':
-    case ')':
-    case '^':
-    case '$':
+    } else {
       return '\\' + match;
     }
-
-    if (keys)
-      keys.push(key);
-
-    return '([^./?#]+)';
   });
 
-  return new RegExp('^' + pattern + '$', 'i');
+  return new RegExp('^' + source + '$', 'i');
 }
 
 module.exports = compileRoute;
