bbdd464797a72673d6cfb44cf5ebb09ade185bc5
Michael Jackson <mjijackson@gmail.com>
2014-11-07 10:50:01 -0800

    Update rewrite module

--- modules/__tests__/rewrite-test.js
+++ modules/__tests__/rewrite-test.js
@@ -1,53 +1,45 @@
 var expect = require('expect');
+var callApp = require('../utils/callApp');
 var rewrite = require('../rewrite');
-var callApp = require('./callApp');
 
-function echoPathInfo(request) {
-  return request.pathInfo;
+function echoPathname(conn) {
+  return conn.pathname;
 }
 
 describe('mach.rewrite', function () {
-  var app = rewrite(echoPathInfo, '/abc', '/xyz');
+  var app = rewrite(echoPathname, '/abc', '/xyz');
   app = rewrite(app, /\/def/g, '/xyz');
   app = rewrite(app, '/abc.jpeg', '/def.jpeg');
 
   describe('GET /abc', function () {
-    beforeEach(function () {
-      return callApp(app, '/abc');
-    });
-
-    it('rewrites the pathInfo properly', function () {
-      expect(lastResponse.buffer).toEqual('/xyz');
+    it('rewrites the pathname properly', function () {
+      return callApp(app, '/abc').then(function (conn) {
+        expect(conn.responseText).toEqual('/xyz');
+      });
     });
   });
 
   describe('GET /def', function () {
-    beforeEach(function () {
-      return callApp(app, '/def');
-    });
-
-    it('rewrites the pathInfo properly', function () {
-      expect(lastResponse.buffer).toEqual('/xyz');
+    it('rewrites the pathname properly', function () {
+      return callApp(app, '/def').then(function (conn) {
+        expect(conn.responseText).toEqual('/xyz');
+      });
     });
   });
 
   describe('GET /def/path/def', function () {
-    beforeEach(function () {
-      return callApp(app, '/def/path/def');
-    });
-
-    it('rewrites the pathInfo properly', function () {
-      expect(lastResponse.buffer).toEqual('/xyz/path/xyz');
+    it('rewrites the pathname properly', function () {
+      return callApp(app, '/def/path/def').then(function (conn) {
+        expect(conn.responseText).toEqual('/xyz/path/xyz');
+      });
     });
   });
 
   describe('when the pattern contains special RegExp chars', function () {
-    beforeEach(function () {
-      return callApp(app, '/abcdjpeg');
-    });
-
-    it('properly escapes those chars', function () {
-      expect(lastResponse.buffer).toEqual('/abcdjpeg');
+    it('rewrites the pathname properly', function () {
+      return callApp(app, '/abcdjpeg').then(function (conn) {
+        expect(conn.responseText).toEqual('/abcdjpeg');
+      });
     });
   });
 });
