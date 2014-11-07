df46054bf2ddfb5b48a0ea5103e97d51c2ec93aa
Michael Jackson <mjijackson@gmail.com>
2014-11-07 09:56:10 -0800

    Update favicon middleware

--- modules/__tests__/favicon-test.js
+++ modules/__tests__/favicon-test.js
@@ -1,6 +1,5 @@
-var assert = require('assert');
 var expect = require('expect');
-var callApp = require('./callApp');
+var callApp = require('../utils/callApp');
 var favicon = require('../favicon');
 
 function ok() {
@@ -9,24 +8,18 @@ function ok() {
 
 describe('mach.favicon', function () {
   describe('when /favicon.ico is requested', function () {
-    beforeEach(function () {
-      return callApp(favicon(ok), '/favicon.ico');
-    });
-
     it('returns 404', function () {
-      assert(lastResponse);
-      expect(lastResponse.status).toEqual(404);
+      return callApp(favicon(ok), '/favicon.ico').then(function (conn) {
+        expect(conn.status).toEqual(404);
+      });
     });
   });
 
   describe('when /favicon.ico?a=b is requested', function () {
-    beforeEach(function () {
-      return callApp(favicon(ok), '/favicon.ico?a=b');
-    });
-
     it('returns 404', function () {
-      assert(lastResponse);
-      expect(lastResponse.status).toEqual(404);
+      return callApp(favicon(ok), '/favicon.ico?a=b').then(function (conn) {
+        expect(conn.status).toEqual(404);
+      });
     });
   });
 });
