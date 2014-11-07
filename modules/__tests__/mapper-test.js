65063f71cb9847d32da920bea6a0a3eaf1359f93
Michael Jackson <mjijackson@gmail.com>
2014-11-07 10:37:36 -0800

    Update mapper middleware

--- modules/__tests__/mapper-test.js
+++ modules/__tests__/mapper-test.js
@@ -1,16 +1,15 @@
-var assert = require('assert');
 var expect = require('expect');
+var callApp = require('../utils/callApp');
 var createMapper = require('../utils/createMapper');
-var callApp = require('./callApp');
 
 describe('mach.mapper', function () {
 
-  function showInfo(request) {
+  function showInfo(conn) {
     return {
       status: 200,
       headers: {
-        'Script-Name': request.scriptName,
-        'Path-Info': request.pathInfo
+        'Basename': conn.basename,
+        'Pathname': conn.pathname
       }
     };
   }
@@ -24,92 +23,49 @@ describe('mach.mapper', function () {
   });
 
   describe('when the request does not match a mapping', function () {
-    beforeEach(function () {
-      return callApp(app, '/three');
-    });
-
     it('passes the request through to the default app', function () {
-      assert(lastResponse);
-      expect(lastResponse.status).toEqual(404);
+      return callApp(app, '/three').then(function (conn) {
+        expect(conn.status).toEqual(404);
+      });
     });
   });
 
   describe('when a request matches a mapping by path', function () {
     describe('and the mapping has a different host', function () {
-      beforeEach(function () {
-        return callApp(app, {
-          headers: {
-            Host: 'example.com'
-          },
-          path: '/two'
-        });
-      });
-
       it('passes the request through to the default app', function () {
-        assert(lastResponse);
-        expect(lastResponse.status).toEqual(404);
+        return callApp(app, 'http://example.com/two').then(function (conn) {
+          expect(conn.status).toEqual(404);
+        });
       });
     });
 
     describe('and the mapping has no host', function () {
-      beforeEach(function () {
-        return callApp(app, '/one/messages');
-      });
-
       it('passes the request through to the mapping', function () {
-        assert(lastResponse);
-        expect(lastResponse.status).toEqual(200);
-      });
-
-      it('puts the matching portion of the URL in the scriptName variable', function () {
-        expect(lastResponse.headers['Script-Name']).toEqual('/one');
-      });
-
-      it('stuffs the remainder of the path into the pathInfo variable', function () {
-        expect(lastResponse.headers['Path-Info']).toEqual('/messages');
+        return callApp(app, '/one/messages').then(function (conn) {
+          expect(conn.status).toEqual(200);
+          expect(conn.response.headers['Basename']).toEqual('/one');
+          expect(conn.response.headers['Pathname']).toEqual('/messages');
+        });
       });
     });
 
     describe('and the mapping has a matching host', function () {
-      beforeEach(function () {
-        return callApp(app, {
-          headers: {
-            Host: 'example.org'
-          },
-          path: '/two/messages'
-        });
-      });
-
       it('passes the request through to the mapping', function () {
-        assert(lastResponse);
-        expect(lastResponse.status).toEqual(200);
-      });
-
-      it('puts the matching portion of the URL in the scriptName variable', function () {
-        expect(lastResponse.headers['Script-Name']).toEqual('/two');
-      });
-
-      it('stuffs the remainder of the path into the pathInfo variable', function () {
-        expect(lastResponse.headers['Path-Info']).toEqual('/messages');
+        return callApp(app, 'http://example.org/two/messages').then(function (conn) {
+          expect(conn.status).toEqual(200);
+          expect(conn.response.headers['Basename']).toEqual('/two');
+          expect(conn.response.headers['Pathname']).toEqual('/messages');
+        });
       });
     });
 
     describe('and there is no remaining path', function () {
-      beforeEach(function () {
-        return callApp(app, '/one');
-      });
-
       it('passes the request through to the mapping', function () {
-        assert(lastResponse);
-        expect(lastResponse.status).toEqual(200);
-      });
-
-      it('puts the matching portion of the URL in the scriptName variable', function () {
-        expect(lastResponse.headers['Script-Name']).toEqual('/one');
-      });
-
-      it('populates the pathInfo variable with /', function () {
-        expect(lastResponse.headers['Path-Info']).toEqual('/');
+        return callApp(app, '/one').then(function (conn) {
+          expect(conn.status).toEqual(200);
+          expect(conn.response.headers['Basename']).toEqual('/one');
+          expect(conn.response.headers['Pathname']).toEqual('/');
+        });
       });
     });
   });
