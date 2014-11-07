b9ad61dbd12d77503a07ea72b2da00c4fcac2ccd
Michael Jackson <mjijackson@gmail.com>
2014-11-07 10:47:45 -0800

    Update router tests

--- modules/__tests__/router-test.js
+++ modules/__tests__/router-test.js
@@ -1,22 +1,17 @@
-var assert = require('assert');
 var expect = require('expect');
+var callApp = require('../utils/callApp');
 var router = require('../router');
-var callApp = require('./callApp');
 
 var UNDEF = '__UNDEFINED__'; // So we can test for undefined.
 
-function stringifyParams(request) {
-  return JSON.stringify(request.params, function (key, value) {
+function stringifyParams(conn) {
+  return JSON.stringify(conn.params, function (key, value) {
     return value === undefined ? UNDEF : value;
   });
 }
 
-function lastParams() {
-  assert(lastResponse.buffer);
-  return JSON.parse(lastResponse.buffer);
-}
-
 describe('mach.router', function () {
+
   var app = router();
 
   app.route('/posts/:id', 'GET', stringifyParams);
@@ -26,113 +21,92 @@ describe('mach.router', function () {
   app.route(/\/users\/(\d+)/i, stringifyParams);
 
   describe('when a match cannot be made', function () {
-    beforeEach(function () {
-      return callApp(app, '/');
-    });
-
     it('returns 404', function () {
-      expect(lastResponse.status).toEqual(404);
+      return callApp(app, '/').then(function (conn) {
+        expect(conn.status).toEqual(404);
+      });
     });
   });
 
   describe('GET /posts/1', function () {
-    beforeEach(function () {
-      return callApp(app, '/posts/1');
-    });
-
     it('has the correct params', function () {
-      expect(lastParams()).toEqual({ id: '1' });
+      return callApp(app, '/posts/1').then(function (conn) {
+        expect(JSON.parse(conn.responseText)).toEqual({ id: '1' });
+      });
     });
   });
 
   describe('POST /posts/2', function () {
-    beforeEach(function () {
-      return callApp(app, { method: 'POST', path: '/posts/2' });
-    });
-
     it('has the correct params', function () {
-      expect(lastParams()).toEqual({ id: '2' });
+      return callApp(app, { method: 'POST', url: '/posts/2' }).then(function (conn) {
+        expect(JSON.parse(conn.responseText)).toEqual({ id: '2' });
+      });
     });
   });
 
   describe('DELETE /posts/3', function () {
-    beforeEach(function () {
-      return callApp(app, { method: 'DELETE', path: '/posts/3' });
-    });
-
     it('has the correct params', function () {
-      expect(lastParams()).toEqual({ id: '3' });
+      return callApp(app, { method: 'DELETE', url: '/posts/3' }).then(function (conn) {
+        expect(JSON.parse(conn.responseText)).toEqual({ id: '3' });
+      });
     });
   });
 
   describe('GET /feeds/5', function () {
-    beforeEach(function () {
-      return callApp(app, '/feeds/5');
-    });
-
     it('has the correct params', function () {
-      expect(lastParams()).toEqual({ id: '5', format: UNDEF });
+      return callApp(app, '/feeds/5').then(function (conn) {
+        expect(JSON.parse(conn.responseText)).toEqual({ id: '5', format: UNDEF });
+      });
     });
   });
 
   describe('GET /feeds/5.html', function () {
-    beforeEach(function () {
-      return callApp(app, '/feeds/5.html');
-    });
-
     it('has the correct params', function () {
-      expect(lastParams()).toEqual({ id: '5', format: 'html' });
+      return callApp(app, '/feeds/5.html').then(function (conn) {
+        expect(JSON.parse(conn.responseText)).toEqual({ id: '5', format: 'html' });
+      });
     });
   });
 
   describe('GET /files/feed.xml', function () {
-    beforeEach(function () {
-      return callApp(app, '/files/feed.xml');
-    });
-
     it('has the correct params', function () {
-      expect(lastParams()).toEqual({ splat: [ 'feed', 'xml' ] });
+      return callApp(app, '/files/feed.xml').then(function (conn) {
+        expect(JSON.parse(conn.responseText)).toEqual({ splat: [ 'feed', 'xml' ] });
+      });
     });
   });
 
   describe('GET /files/feed.', function () {
-    beforeEach(function () {
-      return callApp(app, '/files/feed.');
-    });
-
     it('has the correct params', function () {
-      expect(lastParams()).toEqual({ splat: [ 'feed', '' ] });
+      return callApp(app, '/files/feed.').then(function (conn) {
+        expect(JSON.parse(conn.responseText)).toEqual({ splat: [ 'feed', '' ] });
+      });
     });
   });
 
   describe('GET /files/.xml', function () {
-    beforeEach(function () {
-      return callApp(app, '/files/.xml');
-    });
-
     it('has the correct params', function () {
-      expect(lastParams()).toEqual({ splat: [ '', 'xml' ] });
+      return callApp(app, '/files/.xml').then(function (conn) {
+        expect(JSON.parse(conn.responseText)).toEqual({ splat: [ '', 'xml' ] });
+      });
     });
   });
 
   describe('PUT /posts/1', function () {
-    beforeEach(function () {
-      return callApp(app, { method: 'PUT', path: '/posts/1' });
-    });
-
-    it('returns 404', function () {
-      expect(lastResponse.status).toEqual(404);
+    it('has the correct params', function () {
+      return callApp(app, { method: 'PUT', path: '/posts/1' }).then(function (conn) {
+        expect(conn.status).toEqual(404);
+      });
     });
   });
 
   describe('GET /users/1', function () {
-    beforeEach(function () {
-      return callApp(app, '/users/1');
-    });
-
     it('has the correct params', function () {
-      // Regular expressions don't have named parameters.
-      expect(lastParams()).toEqual({});
+      return callApp(app, '/users/1').then(function (conn) {
+        // Regular expressions don't have named parameters.
+        expect(JSON.parse(conn.responseText)).toEqual({});
+      });
     });
   });
+
 });
