07b8fc906a6522c717e9907f4a4164492b626ba5
Michael Jackson <mjijackson@gmail.com>
2014-11-08 07:52:17 -0800

    Update stream example

--- examples/stream.js
+++ examples/stream.js
@@ -1,23 +1,23 @@
 // This example demonstrates how mach can be used to create a server
 // that streams content back to the client. The best way to see the
-// streaming data is probably using curl or netcat, i.e.
+// streaming data is probably using curl or netcat, e.g.
 // curl http://localhost:5000
 
 var Stream = require('bufferedstream');
 var mach = require('../modules');
 
-mach.serve(function (request, response) {
-  // Set response.content to the stream you want to send.
+mach.serve(function (conn) {
+  // Set conn.response.content to the stream you want to send.
   // In this example, the stream is an infinite stream of
-  // timestamps. In normal usage you'll probably use one
-  // of node's readables (e.g. fs.createReadStream).
-  response.content = new Stream;
+  // timestamps. Typically you'd use one of node's readables
+  // (e.g. fs.createReadStream).
+  var content = conn.response.content = new Stream;
 
   var timer = setInterval(function () {
-    response.content.write((new Date).toString() + '\n');
+    content.write((new Date).toString() + '\n');
   }, 1000);
 
-  request.onClose = function () {
+  conn.onClose = function () {
     console.log('client closed connection');
     clearInterval(timer);
   };
