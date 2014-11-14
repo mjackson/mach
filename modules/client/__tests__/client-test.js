db1265379ec12e31f7946630465a037ef7ce4b39
Daniel Neighman <has.sox@gmail.com>
2014-11-14 10:18:39 -0800

    Adds some tests around the client

--- modules/client/__tests__/client-test.js
+++ modules/client/__tests__/client-test.js
@@ -5,6 +5,11 @@ function echoMethod(conn) {
   return conn.method;
 }
 
+function echoRequestHeaders(_) {
+  return function(conn) { return conn.json(conn.request.headers); };
+}
+
+
 [ 'DELETE',
   'GET',
   'HEAD',
@@ -25,4 +30,60 @@ function echoMethod(conn) {
       });
     });
   });
+});
+
+describe('mach.client', function() {
+  describe('with a custom stack', function() {
+    var stack;
+
+    beforeEach(function() { stack = mach.stack(); });
+    afterEach(function() { stack = null; });
+
+    it('allows access on the outgoing request via the stack', function(done) {
+      stack.use(function(app) {
+        return function(conn) {
+          conn.request.headers['X.Test'] = 'Test Value'; 
+          return app(conn);
+        };
+      });
+      stack.use(echoRequestHeaders);
+
+      mach.get(stack, 'http://example.com/foo').then(function(conn) {
+        expect(conn.method).toEqual('GET');
+        expect(JSON.parse(conn.responseText)['X.Test']).toEqual('Test Value');
+        done();
+      }).catch(done);
+    });
+
+    it('allows access on the outgoing request via a callback', function(done) {
+      stack.use(echoRequestHeaders);
+
+      mach.get(stack, 'http://example.com/foo', function(conn) {
+        conn.request.headers['X.Test'] = 'from callback';
+      }).then(function(conn) {
+        expect(JSON.parse(conn.responseText)['X.Test']).toEqual('from callback');
+
+        // it doesn't keep the function as part of the stack
+        mach.get(stack, 'http://example.com/foo')
+        .then(function(conn2) {
+          expect(JSON.parse(conn2.responseText)['X.Test']).toEqual(undefined);
+          done();
+        }).catch(done);
+      }).catch(done);
+    });
+
+    it('lets me set the body on a post request', function(done) {
+      stack.use(function(app) {
+        return function(conn) { conn.response.content = conn.request.content; };
+      });
+
+      mach.post(stack, 'http://foo.bar', function(conn) {
+        conn.request.content = "Here Yo";
+      }).then(function(conn) {
+        expect(conn.responseText).toEqual('Here Yo');
+        done();
+      }).catch(done);
+    });
+  });
+
 });
