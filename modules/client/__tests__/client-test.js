0415bd0fa868ab299290f3dd53d60ce787e0d330
Michael Jackson <mjijackson@gmail.com>
2014-12-21 22:29:00 -0800

    Style tweaks

--- modules/client/__tests__/client-test.js
+++ modules/client/__tests__/client-test.js
@@ -6,7 +6,9 @@ function echoMethod(conn) {
 }
 
 function echoRequestHeaders(_) {
-  return function(conn) { return conn.json(conn.request.headers); };
+  return function (conn) {
+    return conn.json(conn.request.headers);
+  };
 }
 
 
@@ -32,7 +34,7 @@ function echoRequestHeaders(_) {
   });
 });
 
-describe('mach.client', function() {
+describe('mach.client', function () {
 
   it('may be used with an options object as the first argument', function () {
     // This test may take a while because it makes a real network connection.
@@ -46,51 +48,51 @@ describe('mach.client', function() {
     });
   });
 
-  describe('with a custom stack', function() {
+  describe('with a custom stack', function () {
     var stack;
 
-    beforeEach(function() { stack = mach.stack(); });
-    afterEach(function() { stack = null; });
+    beforeEach(function () { stack = mach.stack(); });
+    afterEach(function () { stack = null; });
 
-    it('allows access on the outgoing request via the stack', function() {
-      stack.use(function(app) {
-        return function(conn) {
+    it('allows access on the outgoing request via the stack', function () {
+      stack.use(function (app) {
+        return function (conn) {
           conn.request.headers['X.Test'] = 'Test Value'; 
           return app(conn);
         };
       });
       stack.use(echoRequestHeaders);
 
-      return mach.get(stack, 'http://example.com/foo').then(function(conn) {
+      return mach.get(stack, 'http://example.com/foo').then(function (conn) {
         expect(conn.method).toEqual('GET');
         expect(JSON.parse(conn.responseText)['X.Test']).toEqual('Test Value');
       });
     });
 
-    it('allows access on the outgoing request via a modifier', function() {
+    it('allows access on the outgoing request via a modifier', function () {
       stack.use(echoRequestHeaders);
 
-      return mach.get(stack, 'http://example.com/foo', function(conn) {
+      return mach.get(stack, 'http://example.com/foo', function (conn) {
         conn.request.headers['X.Test'] = 'from modifier';
-      }).then(function(conn) {
+      }).then(function (conn) {
         expect(JSON.parse(conn.responseText)['X.Test']).toEqual('from modifier');
 
         // it doesn't keep the function as part of the stack
         return mach.get(stack, 'http://example.com/foo')
-        .then(function(conn2) {
+        .then(function (conn2) {
           expect(JSON.parse(conn2.responseText)['X.Test']).toEqual(undefined);
         });
       });
     });
 
-    it('lets me set the body on a post request', function() {
-      stack.use(function(app) {
-        return function(conn) { conn.response.content = conn.request.content; };
+    it('lets me set the body on a post request', function () {
+      stack.use(function (app) {
+        return function (conn) { conn.response.content = conn.request.content; };
       });
 
-      return mach.post(stack, 'http://foo.bar', function(conn) {
+      return mach.post(stack, 'http://foo.bar', function (conn) {
         conn.request.content = "Here Yo";
-      }).then(function(conn) {
+      }).then(function (conn) {
         expect(conn.responseText).toEqual('Here Yo');
       });
     });
