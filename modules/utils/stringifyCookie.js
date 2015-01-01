d61c7623b70510397715071ea52cfc314185bb76
Michael Jackson <mjijackson@gmail.com>
2015-01-01 11:35:52 -0800

    Better stringifyCookie

--- modules/utils/stringifyCookie.js
+++ modules/utils/stringifyCookie.js
@@ -7,7 +7,7 @@
  * - path
  * - expires
  * - secure
- * - httpOnly
+ * - httpOnly or HttpOnly
  */
 function stringifyCookie(name, options) {
   options = options || {};
@@ -17,11 +17,20 @@ function stringifyCookie(name, options) {
 
   var cookie = encodeURIComponent(name) + '=' + encodeURIComponent(options.value || '');
 
-  if (options.domain)   cookie += '; domain=' + options.domain;
-  if (options.path)     cookie += '; path=' + options.path;
-  if (options.expires)  cookie += '; expires=' + options.expires.toUTCString();
-  if (options.secure)   cookie += '; secure';
-  if (options.httpOnly) cookie += '; HttpOnly';
+  if (options.domain)
+    cookie += '; domain=' + options.domain;
+
+  if (options.path)
+    cookie += '; path=' + options.path;
+
+  if (options.expires)
+    cookie += '; expires=' + (options.expires instanceof Date) ? options.expires.toUTCString() : options.expires;
+
+  if (options.secure)
+    cookie += '; secure';
+
+  if (options.httpOnly || options.HttpOnly)
+    cookie += '; HttpOnly';
 
   return cookie;
 }
