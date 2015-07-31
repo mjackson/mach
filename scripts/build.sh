1df7a2e64011f46354cf3107429139b6a0cccd44
Michael Jackson <mjijackson@gmail.com>
2015-07-31 08:24:50 -0700

    Use webpack -p for minified build

--- scripts/build.sh
+++ scripts/build.sh
@@ -9,6 +9,6 @@ babel ./modules -d $build_dir
 find -X $build_dir -type d -name __tests__ | xargs rm -rf
 
 NODE_ENV=production webpack modules/index.js $build_dir/umd/mach.js
-NODE_ENV=production COMPRESS=1 webpack modules/index.js $build_dir/umd/mach.min.js
+NODE_ENV=production webpack -p modules/index.js $build_dir/umd/mach.min.js
 
 echo "gzipped, the UMD build is `gzip -c $build_dir/umd/mach.min.js | wc -c` bytes"
--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,5 @@
 var webpack = require('webpack');
 
-var plugins = [
-  new webpack.DefinePlugin({
-    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-  })
-];
-
-if (process.env.COMPRESS) {
-  plugins.push(
-    new webpack.optimize.UglifyJsPlugin({
-      compressor: {
-        warnings: false
-      }
-    })
-  );
-}
-
 module.exports = {
 
   output: {
@@ -34,6 +18,10 @@ module.exports = {
     process: false
   },
 
-  plugins: plugins
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    })
+  ]
 
 };
